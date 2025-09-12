fb86dca4cacfca2488969047126bf30232c586c0
Lin-Mumu-1 <jyJoshua1_1@163.com>
2025-09-13 03:37:04 +0800

    feat:test run -->step32

--- worksheet2/my-react-app/src/App.test.tsx
+++ worksheet2/my-react-app/src/App.test.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { createRoot } from 'react-dom/client'; // 引入 React 18 的 createRoot
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  const root = createRoot(div); // 创建根节点
+  root.render(<App />); // 渲染组件
+  root.unmount(); // 卸载组件
 });
+
+describe('Addition', () => {
+  it('knows that 2 and 2 make 4', () => {
+    expect(2 + 2).toBe(4);
+  });
+});
+
+describe('Addition', () => {
+  it('knows that 2 and 2 make 4', () => {
+    expect(2 + 2).toBe(5);
+  });
+});
+
+    
