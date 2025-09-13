e8c4db746581fbe14323727c6041624836333c53
Lin-Mumu-1 <jyJoshua1_1@163.com>
2025-09-13 08:39:59 +0800

    no

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom', // 模拟浏览器环境（React 需此配置）
-  moduleNameMapper: {
-    '\\.(css|less|scss)$': 'identity-obj-proxy', // 处理 CSS 模块
-  },
-};
